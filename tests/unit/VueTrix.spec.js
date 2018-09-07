ff92da92b7e882ce0614f73271dca523bfd2e09a
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2018-09-08 02:08:12 +0900

    Update component test

--- tests/unit/VueTrix.spec.js
+++ tests/unit/VueTrix.spec.js
@@ -5,6 +5,9 @@ describe('VueTrix.vue', () => {
   it('renders trix-editor when passed', () => {
     const wrapper = shallowMount(VueTrix)
 
+    // assert the component is rendered
+    expect(wrapper.is(VueTrix)).toBe(true)
+
     // assert the trix-editor is rendered
     expect(wrapper.find('.trix-content').exists()).toBe(true)
   })
