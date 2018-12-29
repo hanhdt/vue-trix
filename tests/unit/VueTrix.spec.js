55fe8d1caceade82909fe22d97e1d40c56f87329
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2018-12-29 23:25:24 +0900

    Add tests of component's attributes

--- tests/unit/VueTrix.spec.js
+++ tests/unit/VueTrix.spec.js
@@ -3,12 +3,30 @@ import VueTrix from '../../src/components/VueTrix.vue'
 
 describe('VueTrix.vue', () => {
   it('renders trix-editor when passed', () => {
-    const wrapper = shallowMount(VueTrix)
+    const component = shallowMount(VueTrix)
 
     // assert the component is rendered
-    expect(wrapper.is(VueTrix)).toBe(true)
+    expect(component.is(VueTrix)).toBe(true)
 
     // assert the trix-editor is rendered
-    expect(wrapper.find('.trix-content').exists()).toBe(true)
+    expect(component.contains('trix-editor')).toBe(true)
+  })
+
+  it('init default props', () => {
+    const component = shallowMount(VueTrix)
+
+    // assert props
+    expect(component.props().inputId).toBe('')
+    expect(component.props().initContent).toBe('')
+    expect(component.props().placeholder).toBe('')
+  })
+
+  it('have attributes when passed', () => {
+    const component = shallowMount(VueTrix)
+
+    // assert attributes
+    expect(component.find('trix-editor').attributes().class).toBe('trix-content')
+    expect(component.find('trix-editor').attributes().role).toBe('textbox')
+    expect(component.find('trix-editor').attributes().placeholder).toBe('')
   })
 })
