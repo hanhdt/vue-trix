98d78eb0c2d506ac6d477a394b453799603ad703
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2018-12-30 11:26:39 +0900

    Remove config duplicates

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   moduleFileExtensions: [
     'js',
+    'jsx',
+    'json',
     'vue'
   ],
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    '/node_modules/.*'
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss)$': 'identity-obj-proxy'
