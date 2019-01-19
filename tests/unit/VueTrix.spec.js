976f5246b1d423506de60b0282cbf4a4832a7447
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2019-01-19 23:22:49 +0900

    Refactor SFC unit tests

--- tests/unit/VueTrix.spec.js
+++ tests/unit/VueTrix.spec.js
@@ -3,41 +3,69 @@ import VueTrix from '../../src/components/VueTrix.vue'
 
 describe('VueTrix.vue', () => {
   it('renders valid elements', () => {
-    const wrapper = shallowMount(VueTrix)
+    const wrapper = mount(VueTrix)
 
     // assert the component is rendered
     expect(wrapper.is(VueTrix)).toBe(true)
+
     // assert the trix-editor is rendered
     expect(wrapper.contains('trix-editor')).toBe(true)
+    expect(wrapper.find('trix-editor').attributes().input).toBeDefined()
+
     // assert the hidden input is rendered
     expect(wrapper.contains('input')).toBe(true)
+    expect(wrapper.find('input').attributes().id).toBeDefined()
   })
 
   it('has initial props', () => {
     const wrapper = shallowMount(VueTrix, {
       propsData: {
-        initContent: 'aaa'
+        inputId: 'inputId',
+        initContent: 'initContent',
+        placeholder: 'placeholder'
       }
     })
 
-    // assert props correctly
-    expect(wrapper.props().inputId).toBe('')
-    expect(wrapper.props().placeholder).toBe('')
+    // assert component props correctly
+    expect(wrapper.props().inputId).toBe('inputId')
+    expect(wrapper.props().placeholder).toBe('placeholder')
+    expect(wrapper.props().initContent).toBe('initContent')
+  })
 
+  it('has valid hidden input', () => {
+    const wrapper = mount(VueTrix, {
+      propsData: {
+        inputId: 'inputId',
+        initContent: 'initContent',
+        placeholder: 'placeholder'
+      }
+    })
+
+    // get hidden input element
     const inputWrapper = wrapper.find('input')
     const inputEl = inputWrapper.element
 
-    // Has the connect starting value
-    expect(inputEl.value).toEqual('aaa')
+    // assert hidden input attributes
+    expect(inputEl.value).toEqual('initContent')
+    expect(inputEl.id).toEqual('inputId')
   })
 
-  it('have attributes when passed', () => {
-    const wrapper = shallowMount(VueTrix)
+  it('has valid trix-editor attributes', () => {
+    const wrapper = mount(VueTrix, {
+      propsData: {
+        inputId: 'inputId',
+        initContent: 'initContent',
+        placeholder: 'placeholder'
+      }
+    })
+
+    // get trix-editor element
+    const trixWrapper = wrapper.find('trix-editor')
 
     // assert attributes
-    expect(wrapper.find('trix-editor').attributes().class).toBe('trix-content')
-    expect(wrapper.find('trix-editor').attributes().role).toBe('textbox')
-    expect(wrapper.find('trix-editor').attributes().placeholder).toBe('')
+    expect(trixWrapper.attributes().class).toBe('trix-content')
+    expect(trixWrapper.attributes().role).toBe('textbox')
+    expect(trixWrapper.attributes().placeholder).toBe('placeholder')
   })
 
   it('works with v-model dirrective', () => {
